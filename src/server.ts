d1028f30313f4776b96af0e08763c4a7f24524e1
Palash Nath <palashnath880@gmail.com>
2024-11-20 21:04:59 +0600

    apollo server init

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,36 @@
-import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express, { Application } from "express";
+import resolvers from "./resolvers";
+import { readFileSync } from "fs";
+import path from "path";
+import bodyParser from "body-parser";
 
-// port
-const PORT = process.env.PORT || 5000;
+// import graphql schema
+const typeDefs = readFileSync(path.join(__dirname, "schema.graphql"), "utf-8");
 
-// create express app
-const app = express();
+async function main() {
+  try {
+    // port
+    const PORT = process.env.PORT || 5000;
 
-// listen server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    // create express app
+    const app = express() as Application;
+
+    app.use(bodyParser.json());
+
+    // create apollo server
+    const server = new ApolloServer({ resolvers, typeDefs });
+    await server.start();
+
+    app.use("/graphql", expressMiddleware(server));
+    // listen server
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
+main().then();
